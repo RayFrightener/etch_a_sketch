41abe5cb1322f2cf9468c3d876d5059a2893e6f9
Pranshu <pranshuchawla19@gmail.com>
2023-08-14 17:07:30 -0400

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch_a_sketch
+Here is where I am going to document the journey. 
+Initial impressions: this seems like a fun project, the end result will be so beautiful. I am excited! 
---
+++ script.js
@@ -0,0 +1,10 @@
+
+const container = document.getElementById("container");
+
+for(let i = 0; i < 256; i++){
+
+    let gridElement = document.createElement("div");
+    container.appendChild(gridElement);
+
+}
+
