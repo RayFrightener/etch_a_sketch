a373e4212b4cf84fae087102d465d149bb6d2781
Pranshu <pranshuchawla19@gmail.com>
2023-08-14 22:56:01 -0400

    button not working

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 
+const button = document.getElementById("button");
+ 
+button.addEventListener("click", () => {
+    const gridSize = prompt("Enter a grid size (up to 100");
+    
+    while(!isValidGridSize){
+        gridSize = prompt("Please enter a valid gridSize: ");
+    }
+
+    function isValidGridSize(size){
+        return !isNaN(size) && parseInt(size) > 0 && parseInt(size) <= 100;
+    }
+    upgradeGrid(gridSize);
+})
+
+function upgradeGrid(gridSize){
+    const container = document.getElementById("container");
+    container.innerHTML = "";
+
+    for (let i=0; i<gridSize; i++){
+        let gridElement= document.createElement("div");
+        container.appendChild(gridElement);
+    
+        gridElement.addEventListener("mouseover",() => {
+            gridElement.style.background ="black";
+        });
+    }
+}
+
 const container = document.getElementById("container");
 
 for(let i = 0; i < 256; i++){
@@ -7,4 +36,11 @@ for(let i = 0; i < 256; i++){
     container.appendChild(gridElement);
 
 }
+
+const gridDivs = document.querySelectorAll("#container > div");
+gridDivs.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+        div.style.background = "black";
+    });
+});
 
